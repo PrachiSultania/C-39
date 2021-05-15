4602ab6dfef0a28595738a228977edaa736ffe52
unknown <prachisultania007@gmail.com>
2021-05-15 19:47:47 +0530

    Added Images

--- sketch.js
+++ sketch.js
@@ -9,6 +9,18 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1Img, car2Img, car3Img, car4Img;
+var trackImg, groundImg;
+
+function preload(){
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+  trackImg = loadImage("images/track.jpg");
+  groundImg = loadImage("images/ground.png");
+
+}
 
 
 function setup(){
@@ -17,6 +29,7 @@ function setup(){
   game = new Game();
   game.getState();
   game.start();
+
 }
 
 
@@ -27,5 +40,9 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
+    
   }
 }
